2bfac2e2aeb2c466fe5de2dffafbe656025e03f9
sahrfearmacarthy <macarthysahralves>
2026-05-25 15:36:08 -0500

    Created base spting Boot project

---
+++ backend-engineering-lab/src/main/java/com/learning/backendengineeringlab/BackendEngineeringLabApplication.java
@@ -0,0 +1,13 @@
+package com.learning.backendengineeringlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineeringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEngineeringLabApplication.class, args);
+    }
+
+}
---
+++ backend-engineering-lab/src/test/java/com/learning/backendengineeringlab/BackendEngineeringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.backendengineeringlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEngineeringLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
